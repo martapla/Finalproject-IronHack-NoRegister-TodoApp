a0c8b5dcdc0f5e35ebca4b91a90b8cc2c28dafbe
martapla <martaplanella@hotmail.com>
2023-12-05 22:55:27 +0100

    config

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,24 @@
-import { fileURLToPath, URL } from 'node:url'
+// import { fileURLToPath, URL } from 'node:url'
 
+// import { defineConfig } from 'vite'
+// import vue from '@vitejs/plugin-vue'
+
+
+// export default defineConfig({
+//   plugins: [
+//     vue(),
+//   ],
+//   resolve: {
+//     alias: {
+//       '@': fileURLToPath(new URL('./src', import.meta.url))
+//     },
+//   },
+  
+// })
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
-
-  build: {
-    rollupOptions: {
-      plugins: [
-        {
-          name: 'replace-imports',
-          resolveId(id) {
-            if (id.endsWith('.jpeg')) {
-              return { id: `path/to/your/placeholder-image.jpeg`, external: true };
-            }
-            return null;
-          },
-        },
-      ],
-    },
-  },
+  plugins: [vue()]
 })
