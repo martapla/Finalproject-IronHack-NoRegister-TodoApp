d087923d4d4ea1a4d4cbc0a580a7385f9cb5916b
martapla <martaplanella@hotmail.com>
2023-12-07 13:25:55 +0100

    vercel changes

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,17 @@
-// import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url'
 
-// import { defineConfig } from 'vite'
-// import vue from '@vitejs/plugin-vue'
-
-
-// export default defineConfig({
-//   plugins: [
-//     vue(),
-//   ],
-//   resolve: {
-//     alias: {
-//       '@': fileURLToPath(new URL('./src', import.meta.url))
-//     },
-//   },
-  
-// })
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 
 export default defineConfig({
-  plugins: [vue()],
-
-  build: {
-    rollupOptions: {
-      external: ['src/assets/avatar2.jpeg'],
+  plugins: [
+    vue(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  
 })
