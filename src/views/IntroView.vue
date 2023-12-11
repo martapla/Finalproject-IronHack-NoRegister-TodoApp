378433177518f0f2bdc60dcb40928cf486f74ece
martapla <martaplanella@hotmail.com>
2023-12-11 19:27:10 +0100

    responsive

--- src/views/IntroView.vue
+++ src/views/IntroView.vue
@@ -2,7 +2,16 @@
   <div class="intro-container">
     <p class="intro-title">Intro </p>
     <p class="intro-description">
-        This is a version of the Final Project from Ironhack. <br/> I have done this version where you can navigate the entire app without the need to register. <br/> The original version is done with Supabase, so it requires your mail and password to access. <br/> You can also find the code in my Github,too.
+        This is a version of the Final Project from Ironhack. <br/> 
+    </p>
+    <p class="intro-description">
+        I have done this version where you can navigate the entire app without the need to register. <br/> 
+    </p>
+    <p class="intro-description">
+       The original version is done with Supabase, so it requires your mail and password to access. <br/> 
+    </p>
+    <p class="intro-description">
+       You can also find the code of both in my Github.
     </p>
   </div>
 </template>
